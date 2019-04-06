96b708e9ee9e90a7d9ef91b453da3174b49cff1d
svyat-pro <41801069+svyat-pro@users.noreply.github.com>
2019-04-06 16:23:18 +0300

    adding files

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from django.shortcuts import reverse
 
-# Create your models here.
+from django.utils.text import slugify
+from time import time
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
+
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateField(auto_now_add=True)
@@ -12,9 +20,15 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return '{}'.format(self.title)
+
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
@@ -24,4 +38,4 @@ class Tag(models.Model):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
 
     def __str__(self):
-        return '{}'.format(self.title)
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Tag
 from django.views.generic import View
+from .utils import ObjectDetailMixin
+from .forms import TagForm, PostForm
 
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
-#def post_detail(request, slug):
- #   post = Post.objects.get(slug__iexact=slug)
-  #  return render(request, 'blog/post_detail.html', context={'post': post})
 
-class PostDetail(View):
-    def get(self, request, slug):
-        post = Post.objects.get(slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create_form.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'blog/post_create_form.html', context={'form': bound_form})
+
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
-class TagDetail(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
-#def tag_detail(request, slug):
-#    tag = Tag.objects.get(slug__iexact=slug)
-#    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
